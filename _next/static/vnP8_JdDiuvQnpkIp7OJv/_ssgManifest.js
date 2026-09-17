ecd62edcbf7fceb97929753203c4e394733db6dd
FwdEngine Deploy <deploy@fwdengine.com>
2026-09-17 16:15:59 -0400

    deploy: static preview 2026-09-17T20:15:59.818Z

---
+++ _next/static/vnP8_JdDiuvQnpkIp7OJv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Findustries\u002F[slug]","\u002Findustries\u002F[slug]\u002Fopengraph-image","\u002Finsights\u002F[slug]","\u002Finsights\u002F[slug]\u002Fopengraph-image"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
